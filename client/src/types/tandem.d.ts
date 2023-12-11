c04bb98bb8679f58a07a5faa7305adb3181d152a
Jan Liska <jan.liska@autodesk.com>
2023-12-11 22:35:21 +0100

    updated type definitions

--- client/src/types/tandem.d.ts
+++ client/src/types/tandem.d.ts
@@ -1,45 +1,41 @@
 declare namespace Autodesk {
-  namespace Viewing {
-    namespace Private {
+  namespace Tandem {
+    interface CompactView {
+      id: string;
+      viewName: string;
+      dashboardName: string;
+      label: string;
+      default: boolean;
+    }
 
-      interface CompactView {
-        id: string;
-        viewName: string;
-        dashboardName: string;
-        label: string;
-        default: boolean;
-      }
-
-      interface View extends CompactView {
-        camera?: any;
-        camera?: cutPlanes;
-        dashboard?: any;
-        createTime?: string;
-      }
+    interface View extends CompactView {
+      camera?: any;
+      camera?: cutPlanes;
+      dashboard?: any;
+      createTime?: string;
+    }
       
-      class DtApp {
-        constructor(options?: any);
+    class DtApp {
+      constructor(options?: any);
 
-        views: DtViews;
+      views: DtViews;
 
-        displayFacility(facility: DtFacility, visibleModelsForView: Set<string> | undefined, viewer: Autodesk.Viewing.GuiViewer3D, forceReload?: boolean): Promise<DtFacility>;
-        getUsersFacilities(): Promise<DtFacility[]>;
-      }
+      displayFacility(facility: DtFacility, visibleModelsForView: Set<string> | undefined, viewer: Autodesk.Viewing.GuiViewer3D, forceReload?: boolean): Promise<DtFacility>;
+      getUsersFacilities(): Promise<DtFacility[]>;
+    }
 
-      class DtFacility {
-        settings: {
-          props: { [key: string]: { [key: string]: string; }};
-        };
-        twinId: string;
+    class DtFacility {
+      settings: {
+        props: { [key: string]: { [key: string]: string; }};
+      };
+      twinId: string;
 
-        getSavedViewsList(): Promise<CompactView[]>;
-      }
+      getSavedViewsList(): Promise<CompactView[]>;
+    }
 
-      class DtViews {
-        fetchFacilityViews(facility: DtFacility): Promise<CompactView[]>;
-        setCurrentView(facility: DtFacility, view: View): Promise<void>;
-      }
-
+    class DtViews {
+      fetchFacilityViews(facility: DtFacility): Promise<CompactView[]>;
+      setCurrentView(facility: DtFacility, view: View): Promise<void>;
     }
   }
 }

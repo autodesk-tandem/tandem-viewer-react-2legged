42c7aef1c0e75d5e543251f6cb6551ca0f7fe20a
Jan Liska <jan.liska@autodesk.com>
2024-03-04 08:30:13 +0100

    updated type definitions

--- client/src/types/tandem.d.ts
+++ client/src/types/tandem.d.ts
@@ -14,23 +14,41 @@ declare namespace Autodesk {
       dashboard?: any;
       createTime?: string;
     }
-      
+
     class DtApp {
       constructor(options?: any);
 
       views: DtViews;
 
       displayFacility(facility: DtFacility, visibleModelsForView: Set<string> | undefined, viewer: Autodesk.Viewing.GuiViewer3D, forceReload?: boolean): Promise<DtFacility>;
+      getCurrentTeamsFacilities(): Promise<DtFacility[]>;
       getUsersFacilities(): Promise<DtFacility[]>;
     }
 
     class DtFacility {
+      modelsList: DtModel[];
       settings: {
+        dateCreated: string;
+        dateModified: string;
+        links: any[];
         props: { [key: string]: { [key: string]: string; }};
       };
       twinId: string;
 
       getSavedViewsList(): Promise<CompactView[]>;
+      waitForAllModels(): Promise<void>;
+    }
+
+    class DtModel {
+      id: number;
+
+      getDbIdsFromElementIds(elementIds: string[]): Promise<number[]>;
+      getData(): any;
+      getElementIdsFromDbIds(dbIds: number[]): Promise<string[]>;
+      getLevels(): Promise<{ [key: string]: any; }>;
+      getPropertiesDt(dbIds: number[], options?: { classificationId?: string; history: boolean?; includeDeleted?: boolean; intersect?: boolean; wantTimeSeries?: boolean; }): Promise<any[]>;
+      getRooms(): Promise<{ [key: string]: any; }>;
+      getTaggedAssets(): Promise<{ cols: any[]; rows: any[]; }>;
     }
 
     class DtViews {

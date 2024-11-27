89054605ca4c53983b1843916773aeb276f36dd3
Jan Liska <jan.liska@autodesk.com>
2024-11-27 23:14:35 +0100

    updated type definitions

--- client/src/types/tandem.d.ts
+++ client/src/types/tandem.d.ts
@@ -1,5 +1,12 @@
 declare namespace Autodesk {
   namespace Tandem {
+    // events
+    const DT_CURRENT_VIEW_CHANGED_EVENT = 'dtCurrentViewChanged';
+    const DT_FACETS_LOADED = 'dtFacetsLoaded';
+    const DT_STREAM_MARKER_MOUSE_OVER = 'dtStreamMarkerMouseOver';
+    const DT_STREAM_MARKER_MOUSE_OUT = 'dtStreamMarkerMouseOut';
+    const DT_STREAM_MARKER_CHANGED_EVENT = 'dtStreamMarkerChanged'
+
     interface CompactView {
       id: string;
       dashboardName: string;
@@ -21,6 +28,8 @@ declare namespace Autodesk {
       currentFacility: DtFacility;
       views: DtViews;
 
+      addEventListener(event: string, callback: (event: any) => void): void;
+
       displayFacility(facility: DtFacility, visibleModelsForView: Set<string> | undefined, viewer: Autodesk.Viewing.GuiViewer3D, forceReload?: boolean): Promise<DtFacility>;
       getCurrentTeamsFacilities(): Promise<DtFacility[]>;
       getFacility(urn: string): Promise<DtFacility>;
@@ -38,10 +47,12 @@ declare namespace Autodesk {
         links: any[];
         props: { [key: string]: { [key: string]: string; }};
       };
-      systemsManager: any;
       twinId: string;
+      viewer: Autodesk.Viewing.GuiViewer3D;
 
+      getModels(skipDefault?: boolean): DtModel[];
       getSavedViewsList(): Promise<CompactView[]>;
+      getVisibleElementsByURN(skipDefault?: boolean): { [key: string]: number[]; };
       unloadModels(): void;
       urn(): string;
       waitForAllModels(): Promise<void>;
@@ -54,9 +65,11 @@ declare namespace Autodesk {
       getData(): any;
       getElementIdsFromDbIds(dbIds: number[]): Promise<string[]>;
       getLevels(): Promise<{ [key: string]: any; }>;
-      getPropertiesDt(dbIds: number[], options?: { classificationId?: string; history: boolean?; includeDeleted?: boolean; intersect?: boolean; wantTimeSeries?: boolean; }): Promise<any[]>;
+      getPropertiesDt(dbIds: number[], options?: { classificationId?: string; history?: boolean; includeDeleted?: boolean; intersect?: boolean; wantTimeSeries?: boolean; }): Promise<any[]>;
       getRooms(): Promise<{ [key: string]: any; }>;
       getTaggedAssets(): Promise<{ cols: any[]; rows: any[]; }>;
+      getVisibleDbIds(): number[];
+      waitForLoad(waitForFragments: boolean, waitForInstanceTree: boolean): Promise<void>;
     }
 
     class DtTeam {
@@ -70,6 +83,8 @@ declare namespace Autodesk {
 
     class DtViews {
       currentView: CompactView;
+
+      addEventListener(event: string, callback: (event: any) => void): void;
 
       fetchFacilityViews(facility: DtFacility): Promise<CompactView[]>;
       setCurrentView(facility: DtFacility, view: View): Promise<void>;
